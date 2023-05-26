d2621a48d660e86d2d6e500ad1044a5ef2438f9c
Natan <natanaelmoreirarocha1@gmail.com>
2023-05-26 15:37:37 -0300

    estruturação do pj completo

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -10,6 +10,9 @@ export class SmallCardComponent implements OnInit {
   photoCover:string = "https://legadodamarvel.com.br/wp-content/uploads/2022/02/series-originais-da-marvel-na-netflix-podem-estar-deixando-a-plataforma-legadodamarvel.jpg";
   @Input()
   cardTitle:string = "NOVA SERIE ANUNCIADA"
+
+  @Input()
+  Id:string ="0"
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title":"Novo filme de Pantera negra será lançado em Abril",
+        "description": "Pantera Negra segue T'Challa que, após a morte de seu pai, volta para casa numa isolada e tecnologicamente avançada nação africana para triunfar e assumir seu lugar como rei. Mas, quando um poderoso antigo inimigo reaparece, o poder de T'Challa como rei - e Pantera Negra - é testado ao ser atraído para um formidável conflito que coloca o destino de Wakanda e de todo mundo em risco.",
+        "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/F6482D58C8BB4D899FA4682BF27652C3E1BB73F5971E5F963994112F26A73690/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    },
+      {
+        "id": "2",
+        "title": "O Rei Leão",
+        "description": "Simba, um jovem leão destinado a se tornar rei, é injustamente exilado por seu tio Scar. Com a ajuda de seus amigos, Simba precisa encontrar seu lugar no mundo e retomar o trono que é seu por direito.",
+        "photoCover": "https://i.ytimg.com/vi/8WexnIN2iPg/maxresdefault.jpg"
+      },
+      {
+        "id": "3",
+        "title": "Harry Potter e a Pedra Filosofal",
+        "description": "Harry Potter descobre que é um bruxo e começa a frequentar a Escola de Magia e Bruxaria de Hogwarts. Lá, ele enfrenta desafios e perigos enquanto desvenda os mistérios do passado de seus pais.",
+        "photoCover": "https://www.correiodopovo.com.br/image/policy:1.675086:1629222246/Harry%20Potter%20e%20a%20Pedra%20Filosofal.jpg?a=2%3A1&$p$a=609b4fe"
+      },
+      {
+        "id": "4",
+        "title": "Interestelar",
+        "description": "Em um futuro onde a Terra está à beira da destruição, um grupo de exploradores embarca em uma missão espacial para encontrar um novo lar para a humanidade. Eles enfrentam desafios científicos e emocionais que testam os limites do que é possível.",
+        "photoCover": "https://i0.wp.com/jornal.usp.br/wp-content/uploads/2023/04/Interstellar_resized.png?fit=800%2C420&ssl=1"
+      }
+]
