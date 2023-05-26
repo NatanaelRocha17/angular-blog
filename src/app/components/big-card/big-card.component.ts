8c75448a556e68fae4d57058449a399670a9825a
Natan <natanaelmoreirarocha1@gmail.com>
2023-05-26 09:18:26 -0300

    componentes com propriedades dinamicas

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./big-card.component.css']
 })
 export class BigCardComponent implements OnInit {
+  /*esses valores podem ser inputados --> passado para cá, não eu que irei colocar, para isso eu coloco o @Input */
+  /*E eu inputo as informações de onde meu componente <app-big-card> está sendo chamado --> pages*/
+  @Input()
+  photoCover:string = ""
+  @Input()
+  cardTitle:string = ""
+  @Input()
+  cardDescription:string = ""
 
   constructor() { }
 
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./small-card.component.css']
 })
 export class SmallCardComponent implements OnInit {
-
+  @Input()
+  photoCover:string = "https://legadodamarvel.com.br/wp-content/uploads/2022/02/series-originais-da-marvel-na-netflix-podem-estar-deixando-a-plataforma-legadodamarvel.jpg";
+  @Input()
+  cardTitle:string = "NOVA SERIE ANUNCIADA"
   constructor() { }
 
   ngOnInit(): void {
